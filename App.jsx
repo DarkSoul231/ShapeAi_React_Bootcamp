b14a156294d211d1e224a5d163b0ecbd2ae7ea99
Omkar Bhusnale <73767886+DarkSoul231@users.noreply.github.com>
2021-06-25 16:54:30 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React from "react"; 
+import Header from "./Header";
+import Footer from "./Footer";
+import Banner from "./Banner";
+
+function App(){
+  return(
+    <div>
+      <Header />
+
+      <Banner />
+      <Banner />
+      <Banner />
+      <Banner />
+      
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Banner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Banner(){
+  return(
+    <div className="note">
+      <h1>JavaScript and React.js</h1>
+      <p>A Basic Web Dev React Js Bootcamp..</p>
+    </div>
+  );
+}
+
+export default Banner;
